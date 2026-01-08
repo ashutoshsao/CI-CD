59cb7e446c6a80a8ee75f672436a9e187f67157a
ashutoshsao <ashutoshsao17@gmail.com>
2026-01-09 02:47:20 +0530

    add deploy.sh

---
+++ deploy.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+# Stop script on errors
+set -e
+
+echo "Starting deployment..."
+
+cd /home/ubuntu/goServer
+
+echo "Pulling latest changes..."
+git pull origin main
+
+echo "Downloading dependencies..."
+go mod tidy
+
+# builds and overrides the 'api' binary file
+echo "Building binary..."
+go build -o api main.go
+
+echo "Restarting Systemd service..."
+sudo systemctl restart goserver
+
+echo "Deployment finished successfully!"
