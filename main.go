9a2a3d34fe8b85dbb6e0046ba106b27ac8c9dfcc
ashutoshsao <ashutoshsao17@gmail.com>
2026-01-09 05:03:04 +0530

    add err log

--- main.go
+++ main.go
@@ -1,15 +1,20 @@
 package main
 
 import (
-	"fmt"
-	"net/http"
+    "fmt"
+    "log"
+    "net/http"
 )
 
 func main() {
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintf(w, "hello from GoLang from aws")
-	})
+    http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+        fmt.Fprintf(w, "hello from GoLang from aws")
+    })
 
-	fmt.Println("Server is running on http://localhost:8080")
-	http.ListenAndServe(":8080", nil)
+    fmt.Println("Server is running on http://localhost:8080")
+    
+    // This 'log.Fatal' is the critical part!
+    if err := http.ListenAndServe(":8080", nil); err != nil {
+        log.Fatal(err)
+    }
 }
